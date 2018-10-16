ececaf088f1c1e37b7cf037a0d430c53a28d63f6
Marc-Andre Gatien <m.gatien@gmail.com>
2018-10-16 11:52:20 -0400

    change to public repo to build deb package with bionic support

--- stackdriver/package-build.sh
+++ stackdriver/package-build.sh
@@ -10,22 +10,21 @@ if [ "x$PKGFORMAT" == "xdeb" ]
 then
     # debian denotes 64 arches with amd64
     [ "x$ARCH" == "xx86_64" ] && ARCH="amd64" || true
-    git clone git@github.com:Stackdriver/agent-deb.git --branch $BRANCH
-    pushd agent-deb
+    git clone git@github.com:Stackdriver/agent-packaging.git --branch $BRANCH
+    pushd agent-packaging
+    mv deb/* .
+    mv collectd.conf debian/
     make clean
     make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build || exit $?
     popd
-    [ -d result ] && rm -rf result || true
-    cp -r agent-deb/result .
 elif [ "x$PKGFORMAT" == "xrpm" ]
 then
-    git clone git@github.com:Stackdriver/agent-rpm.git --branch $BRANCH
-    pushd agent-rpm
+    git clone git@github.com:Stackdriver/agent-packaging.git --branch $BRANCH
+    pushd agent-packaging
+    mv rpm/* .
     make clean
     make DISTRO="$DISTRO" ARCH="$ARCH" VERSION="$VERSION" BUILD_NUM="$BUILD_NUM" build || exit $?
     popd
-    [ -d result ] && rm -rf result || true
-    cp -r agent-rpm/result .
 else
     echo "I don't know how to handle label '$PKGFORMAT'. Aborting build"
     exit 1
